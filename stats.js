6fc53ab2d65a44efec9a2a79fedb7e4ce0eaa208
Kisat0 <ghanembenyoucef@gmail.com>
2022-11-13 17:31:01 +0100

    update bug fix stats

--- stats.js
+++ stats.js
@@ -9,32 +9,38 @@ function stat(min, max, step, nbr) {
     while (size <= max) {
 
         for (let i = 0; i < nbr; i++) {
+            var tab1 = [];
+            var tab2 = [];
+            var tab3 = [];
+            var tab4 = [];
 
-            var tab = [];
 
-                for (let index = 0; index < size; index++) {
-                    var rand = Math.floor(Math.random() * 100); 
-                    tab[index] = rand;
-                }
-                countM1 += triBulleOpti(tab);
-                countM2 += triBulle(tab);
-                countM3 += tri_selection(tab);
-                countM4 += triInsertion(tab);
+            for (let index = 0; index < size; index++) {
+                var rand = Math.floor(Math.random() * 100);
+                tab1[index] = rand;
+                tab2[index] = rand;
+                tab3[index] = rand;
+                tab4[index] = rand;
             }
-            countM1 = countM1 / nbr; 
-            countM2 = countM2 / nbr; 
-            countM3 = countM3 / nbr;
-            countM4 = countM4 / nbr;
-            displayStats(size,countM1,countM2,countM3,countM4);
-            console.log(`Taille du Tableau: ${size}, Moyenne Tri à Bulle Opti: ${countM1}`);
-            console.log(`Taille du Tableau: ${size}, Moyenne Tri à Bulle: ${countM2}`);
-            console.log(`Taille du Tableau: ${size}, Moyenne Tri par Selection: ${countM3}`);
-            console.log(`Taille du Tableau: ${size}, Moyenne Tri par Insertion: ${countM4}`);
-         
+
+            countM1 += triBulleOpti(tab1);
+            countM2 += triBulle(tab2);
+            countM3 += tri_selection(tab3);
+            countM4 += triInsertion(tab4);
+        }
+        countM1 = countM1 / nbr;
+        countM2 = countM2 / nbr;
+        countM3 = countM3 / nbr;
+        countM4 = countM4 / nbr;
+        /*   displayStats(size,countM1,countM2,countM3,countM4); */
+        console.log(`Taille du Tableau: ${size}, Moyenne Tri à Bulle Opti: ${countM1}`);
+        console.log(`Taille du Tableau: ${size}, Moyenne Tri à Bulle: ${countM2}`);
+        console.log(`Taille du Tableau: ${size}, Moyenne Tri par Selection: ${countM3}`);
+        console.log(`Taille du Tableau: ${size}, Moyenne Tri par Insertion: ${countM4}`);
+
         size += step;
     }
     console.log("Function finished");
 }
 
 
-stat(10, 20, 5, 10);
