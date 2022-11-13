32decac040ac322c9fecea5087eafddc5f147a98
Kisat0 <ghanembenyoucef@gmail.com>
2022-11-13 13:10:52 +0100

    add algo for fonction stats

--- stats.js
+++ stats.js
@@ -1,32 +1,32 @@
-function stat(min,max,step,nbr) {
-    
+function stat(min, max, step, nbr) {
+
     var size = min;
     var countM1 = 0;
     var countM2 = 0;
     var countM3 = 0;
     var countM4 = 0;
-    
+
     while (size <= max) {
-        
+
         for (let i = 0; i < nbr; i++) {
 
-                var tab = [];
+            var tab = [];
 
-                for (let index = 0; index < size; index++) {
-                    var rand = Math.floor(Math.random() * 100); 
-                    tab[index] = rand;
-                }
-                countM1 = countM1 + triBulleOpti(tab);
-                countM2 += triBulle(tab);
-                countM3 += tri_selection(tab);
+            for (let index = 0; index < size; index++) {
+                var rand = Math.floor(Math.random() * 100);
+                tab[index] = rand;
             }
-            countM1 = countM1 / nbr; 
-            countM2 = countM2 / nbr; 
-            console.log(`Taille du Tableau: ${size}, Moyenne Tri à Bulle Opti: ${countM1}`);
-            console.log(`Taille du Tableau: ${size}, Moyenne Tri à Bulle: ${countM2}`);
-            console.log(`Taille du Tableau: ${size}, Moyenne Tri à Bulle: ${countM3}`);
-         
+            countM1 += triBulleOpti(tab);
+            countM2 += triBulle(tab);
+            countM3 += tri_selection(tab);
+        }
+        countM1 = countM1 / nbr;
+        countM2 = countM2 / nbr;
+        countM3 = countM3 / nbr;
 
+        console.log(`Taille du Tableau: ${size}, Moyenne Tri à Bulle Opti: ${countM1}`);
+        console.log(`Taille du Tableau: ${size}, Moyenne Tri à Bulle: ${countM2}`);
+        console.log(`Taille du Tableau: ${size}, Moyenne Tri à Bulle: ${countM3}`);
 
         size += step;
     }
@@ -34,4 +34,4 @@ function stat(min,max,step,nbr) {
 }
 
 
-stat(10,20,5,10);
+stat(10, 20, 5, 10);
